37faef77a35c0e201eda36694db3e7ec1679f214
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-09-30 21:03:58 +0300

    add class active programmatically

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const slydes = document.querySelectorAll('.slide');
-console.log(slydes);
-const clearClassesActive = () => {
-  slydes.forEach(slyde => slyde.classList.remove('active'))
-}
-slydes.forEach(slyde => {
-  slyde.addEventListener('click', ()=> {
-    clearClassesActive()
-    slyde.classList.add('active')
+const slaidersPlugin = (activeSliderIndex = 0) => {const slydes = document.querySelectorAll('.slide');
+  if(activeSliderIndex < slydes.length) {
+    slydes[activeSliderIndex].classList.add('active')
+  } else slydes[0].classList.add('active');
+
+
+  const clearClassesActive = () => {
+    slydes.forEach(slyde => slyde.classList.remove('active'))
+  }
+
+  slydes.forEach(slyde => {
+    slyde.addEventListener('click', ()=> {
+      clearClassesActive()
+      slyde.classList.add('active')
+    })
   })
-})
+};
+
+slaidersPlugin(2);
